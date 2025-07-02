f367c9ff219fa2794580436d6243d5e0cb15c02d
MhdAqeel <mohamedaqeel1130@gmail.com>
2025-07-02 23:40:48 +0530

    modified:   Date.class 	modified:   Date.java     modified:   TestDate.class 	modified:   TestDate.java

--- Date.java
+++ Date.java
@@ -40,4 +40,61 @@ public class Date {
     public String toString(){
         return "Date is : "+this.getDay()+" / "+this.getMonth()+" / "+this.getYear();
     }
+
+    public String findTommorrow() {
+        int tempyear = this.getYear();
+        int tempmonth = this.getMonth();
+        int tempday = this.getDay();
+        
+    
+        boolean leapYear;
+        if ((tempyear % 400) == 0) {
+            leapYear=true;
+        } else if ((tempyear % 100) == 0) {
+            leapYear=false;
+        } else if ((tempyear % 4) == 0) {
+            leapYear=true;
+        } else {
+            leapYear=false;
+        } 
+
+        switch (tempmonth) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                if (tempday == 31) {
+                    tempday = 1;
+                    tempmonth = (tempmonth % 12) + 1;
+                    if (tempmonth == 1)
+                        tempyear ++;
+                } else {
+                    tempday++;
+                }
+                break;
+            case 4: case 6: case 9: case 11:
+                if (tempday == 30){
+                    tempday = 1;
+                    tempmonth ++;
+                }
+                else{
+                    tempday++;
+                }
+                break;
+            case 2:
+                if (leapYear && tempday ==29){
+                    tempday = 1;
+                    tempmonth++;
+                }
+                else if(!leapYear && tempday == 28){
+                    tempday = 1;
+                    tempmonth++;
+                }
+                else {
+                    tempday++;
+                }
+                break;
+            default:
+                tempday++; 
+                break;
+        }
+        return "Date is : "+tempday+" / "+ tempmonth +" / "+tempyear +" leapyear ="+leapYear;
+    }
 }
--- TestDate.java
+++ TestDate.java
@@ -1,7 +1,9 @@
 public class TestDate {
     public static void main(String[] args) {
         Date millanium = new Date();
-
+        Date tommorrwdate = new Date(2024,2,28);
+        
         System.out.println(millanium.toString());
+        System.out.println(tommorrwdate.findTommorrow());
     }
 }
