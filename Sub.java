cababc8df5789ce0e5b23bc147671fe6fb9e9ded
Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2025-07-11 22:37:56 +0530

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,15 @@
+public class Sub         
+{
+	public static void main(String[]args)
+	{
+	String s1="Central Processing Unit";
+	
+	String s2=s1.substring(5,10);
+	String s3=s1.substring(8);
+	String s4=s1.substring(0,12);
+	
+	System.out.println(s2);
+	System.out.println(s3);
+	System.out.println(s4);
+	}
+}
